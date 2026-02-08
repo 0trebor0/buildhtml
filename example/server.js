a7b9e66cc9cc2aa9479af8c9a2882eb7a1988543
0trebor0 <webdevme@outlook.com>
2026-02-08 12:45:29 +0000

    update

---
+++ example/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const { Document } = require('../index.js');
+
+const server = http.createServer((req, res) => {
+  const doc = new Document();
+  doc.title('LightRender Example');
+  const h1 = doc.create('h1').text('Welcome to LightRender');
+  const p = doc.create('p').text('Zero-dependency, ultra-fast SSR.');
+  doc.use(h1).use(p);
+  const html = doc.render();
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
